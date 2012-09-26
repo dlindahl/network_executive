b7672a065434f00660831c3f865114b1099d9b6b
Derek Lindahl <dlindahl@customink.com>
2012-09-26 17:05:10 -0400

    Added better console debugging

--- app/assets/javascripts/network_executive/set_top_box.js
+++ app/assets/javascripts/network_executive/set_top_box.js
@@ -3,7 +3,7 @@
   var channel = window.location.pathname.split('/')[2];
 
   if( channel ) {
-    console.log( channel );
+    console.log( 'tune in to channel:', channel );
 
     document.getElementById('smpte_message').innerHTML = 'Establishing uplink...';
 
@@ -16,22 +16,25 @@
     }, false);
 
     source.addEventListener('message', function(e) {
+      console.log( '[message]', e.data );
+
       var payload = JSON.parse( e.data );
 
-      console.log('[message]', payload);
-
       document.getElementById('smpte').style.display = 'none';
+
       document.getElementById('program').setAttribute('src', payload.url);
     }, false);
 
     source.addEventListener('error', function(e) {
       if (e.eventPhase == EventSource.CLOSED) {
-        console.log('[error]', e);
+        console.log('[closed]', e);
         document.getElementById('smpte_message').innerHTML = 'Transmission lost...';
         document.getElementById('program').setAttribute('src', 'about:blank');
         // Move this functionality to a class for better cross-browser support
         document.getElementById('smpte').style.display = '-webkit-box';
         // Connection was closed.
+      } else {
+        console.log('[closed]', e);
       }
     }, false);
   } else {
