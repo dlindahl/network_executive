26cff8ed5e7d502268a0ab0f79e022508f3e8e61
Derek Lindahl <dlindahl@customink.com>
2012-09-27 16:23:21 -0400

    Used named routes for OSD links

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 NetworkExecutive::Engine.routes.draw do
   mount NetworkExecutive::Station => '/tune_in', as:'station'
 
-  match 'channels/:channel_name' => 'network#index'
+  match 'channels/:channel_name' => 'network#index', as: :channel
 
   root to:'network#index'
 end
