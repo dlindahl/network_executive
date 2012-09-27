5c336da4dc19c56954e6cdf1265bfc8a57bf2314
Derek Lindahl <dlindahl@customink.com>
2012-09-26 22:50:16 -0400

    Refactored Network to remove Goliath dependency

--- app/models/network_executive/network.rb
+++ app/models/network_executive/network.rb
@@ -1,75 +1,10 @@
-require 'network_executive/plugins/producer'
-require 'network_executive/lineup'
-
 module NetworkExecutive
-  class Network < Goliath::API
+  class Network
 
-    # TODO: Make this work
-    # plugin NetworkExecutive::Producer
-
-    use Rack::Static,
-      index: 'index.html',
-      urls:  [ '/index.html', '/stylesheets', '/javascripts' ],
-      root:  File.join( NetworkExecutive.root, 'public' )
-
-    # TODO: Add test
-    def render( path )
-      path = File.join( NetworkExecutive.root, path )
-
-      if File.exists? path
-        file = File.read path
-
-        [ 200, {}, file ]
-      else
-        raise Goliath::Validation::NotFoundError
-      end
-    end
-
-    def response( env )
-      controller, id = env['PATH_INFO'].split('/')[1..-1]
-
-      case controller
-      when 'tune_in'
-        Viewer.change_channel id, env
-      when 'channels'
-        render 'public/index.html'
-      when 'lineup'
-        [ 200, { 'Content-Type' => 'application/json' }, Lineup.new.to_json ]
-      else
-        raise Goliath::Validation::NotFoundError
-      end
-    end
-
-    class << self
-      def channels=( channel )
-        self.channels << channel
-      end
-
-      def channels
-        @channels ||= []
-      end
-
-      def programming=( program )
-        self.programs << program
-      end
-
-      def programming
-        @programs ||= []
-      end
-
-      def tune_in_to( channel_name, env )
-        channel = self.channels.find do |c|
-          c.name == channel_name
-        end
-
-        raise ChannelNotFound unless channel
-
-        # TODO: Move this to the channel#tune_in
-        channel.subscribe do |msg|
-          env.stream_send "data: #{msg}\n\n"
-        end
-      end
-    end
+    cattr_accessor :channels, :programming
 
   end
+
+  Network.channels    = []
+  Network.programming = []
 end
--- spec/models/network_spec.rb
+++ spec/models/network_spec.rb
@@ -1,80 +1,11 @@
 describe NetworkExecutive::Network do
-  include Goliath::TestHelper
 
-  let(:err) { Proc.new { fail 'API request failed' } }
-
-  it 'should respond to /' do
-    FileUtils.mkdir_p 'public'
-    File.open('public/index.html', 'w') {|f| f.write('') }
-
-    with_api( described_class ) do
-      get_request( { path:'/' }, err ) do |c|
-        c.response_header.should include 'CONTENT_TYPE' => 'text/html'
-      end
-    end
+  it 'should maintain a collection of channels' do
+    described_class.channels.should be_a Array
   end
 
-  it 'should respond to /index.html' do
-    FileUtils.mkdir_p 'public'
-    File.open('public/index.html', 'w') {|f| f.write('') }
-
-    with_api( described_class ) do
-      get_request( { path:'/index.html' }, err ) do |c|
-        c.response_header.should include 'CONTENT_TYPE' => 'text/html'
-      end
-    end
+  it 'should maintain a collection of programs' do
+    described_class.programming.should be_a Array
   end
 
-  it 'should respond to /stylesheets/test.css' do
-    FileUtils.mkdir_p 'public/stylesheets'
-    File.open('public/stylesheets/test.css', 'w') {|f| f.write('') }    
-
-    with_api( described_class ) do
-      get_request( { path:'/stylesheets/test.css' }, err ) do |c|
-        c.response_header.should include 'CONTENT_TYPE' => 'text/css'
-      end
-    end
-  end
-
-  it 'should respond to /javascripts/test.js' do
-    FileUtils.mkdir_p 'public/javascripts'
-    File.open('public/javascripts/test.js', 'w') {|f| f.write('') }    
-
-    with_api( described_class ) do
-      get_request( { path:'/javascripts/test.js' }, err ) do |c|
-        c.response_header.should include 'CONTENT_TYPE' => 'application/javascript'
-      end
-    end
-  end
-
-  it 'should respond to /tune_in/[CHANNEL_NAME]' do
-    NetworkExecutive::Viewer.stub(:change_channel).and_return [200,nil,nil]
-    NetworkExecutive::Viewer.should_receive( :change_channel ).with 'foo', anything()
-
-    with_api( described_class ) do
-      get_request( { path:'/tune_in/foo' }, err )
-    end
-  end
-
-  describe '.tune_in_to' do
-    it 'should subscribe to the channel' do
-      channel_double = double('channel')
-
-      described_class.channels.stub(:find).and_return channel_double
-
-      channel_double.should_receive :subscribe
-
-      described_class.tune_in_to 'my_channel', {}
-    end
-  end
-
-  it 'should respond to /lineup' do
-    NetworkExecutive::Lineup.should_receive( :new ).and_return double('lineup').as_null_object
-
-    with_api( described_class ) do
-      get_request( { path:'/lineup' }, err ) do |c|
-        c.response_header.should include 'CONTENT_TYPE' => 'application/json'
-      end
-    end
-  end
 end
